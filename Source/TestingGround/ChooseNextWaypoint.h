02da467c2c9e0764489f6c370172a15b54fc6462
Kekerjaan-PC\Darksun <asam.nation@gmail.com>
2018-09-24 08:00:54 +0800

    Reading Blackboard Data in C++

--- Source/TestingGround/ChooseNextWaypoint.h
+++ Source/TestingGround/ChooseNextWaypoint.h
@@ -13,7 +13,12 @@ UCLASS()
 class TESTINGGROUND_API UChooseNextWaypoint : public UBTTaskNode
 {
 	GENERATED_BODY()
-	
-	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
-	
+
+		virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+protected:
+
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	struct FBlackboardKeySelector IndexKey;
+
 };

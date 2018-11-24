d3460d575a2971963bc78e1932c6faf514b3d866
Kekerjaan-PC\Darksun <asam.nation@gmail.com>
2018-11-24 17:25:21 +0800

    Spawning Into Free Space

--- Source/TestingGround/Terrain/Tile.h
+++ Source/TestingGround/Terrain/Tile.h
@@ -17,7 +17,7 @@ public:
 	ATile();
 
 	UFUNCTION(BlueprintCallable, Category = "Weapon")
-	void PlaceActors(TSubclassOf<AActor>ToSpawn, int32 MinSpawn, int32 MaxSpawn);
+	void PlaceActors(TSubclassOf<AActor>ToSpawn, int32 MinSpawn = 1, int32 MaxSpawn = 1, float Radius = 500);
 
 protected:
 	// Called when the game starts or when spawned
@@ -29,6 +29,10 @@ public:
 
 private:
 
-	bool CastSphere(FVector Location, float Radius);
+	bool CanSpawnAtLocation(FVector Location, float Radius);
+
+	bool FindEmptyLocation(FVector& OutLocation, float Radius);
+
+	void PlaceActor(TSubclassOf<AActor>ToSpawn, FVector SpawnPoint);
 	
 };
